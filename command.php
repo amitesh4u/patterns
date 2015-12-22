2a239a86688558008e89dd5462e6dd2861e20d24
Yurii Pereskokov <skulines@mail.ru>
2015-12-22 09:25:25 +0500

    '22.12/1'

--- command.php
+++ command.php
@@ -1,10 +1,16 @@
 <?php
 /**
- * 25:02
+ * 39:02
  */
 /**
  * command.php
  * Реализация паттерна Команда
+ *
+ * Паттерн Команда инкапсулирует запрос в виде объекта,
+ * позволяя параметризировать клиентские
+ * запросы другими объектами,
+ * с возможностью отмены операции и
+ * поддержкой состояния
  *
  * @author      Pereskokov Yurii
  * @copyright   2015 Pereskokov Yurii
@@ -24,7 +30,7 @@ class LightsCommand implements ICommand
         echo 'Свет включен';
     }
 
-    public function _toString()
+    public function __toString()
     {
         return 'Включить свет';
     }
@@ -37,7 +43,7 @@ class TvCommand implements ICommand
         echo 'Телевизор включен';
     }
 
-    public function _toString()
+    public function __toString()
     {
         return 'Включить телевизор';
     }
@@ -50,11 +56,25 @@ class MusicCommand implements ICommand
         echo 'Музыка включена';
     }
 
-    public function _toString()
+    public function __toString()
     {
-        return 'Включить свет';
+        return 'Включить музыку';
     }
 }
+
+class TeapotCommand implements ICommand
+{
+    public function execute()
+    {
+        echo 'Чайник включен';
+    }
+
+    public function __toString()
+    {
+        return 'Включить чайник';
+    }
+}
+
 
 class RemoteControl
 {
@@ -62,38 +82,21 @@ class RemoteControl
 
     function __construct()
     {
-        $this->commands['1'] = new LightsCommand();
-        $this->commands['2'] = new TvCommand();
-        $this->commands['3'] = new MusicCommand();
     }
 
     public function drawMenu()
     {
         echo 'Выберете вариант ниже:' . '<br />';
-        echo '1 - Включить свет' . '<br />';
-        echo '1off - выключить свет' . '<br />';
-        echo '2 - Включить телевизор' . '<br />';
-        echo '2off - Выключить телевизор' . '<br />';
-        echo '3 Включить музыку' . '<br />';
-        echo '3off - Выключить музыку' . '<br />';
-        echo '0 - Выход' . '<br />';
+
+        foreach ($this->commands as $key => $command) {
+            echo $key . ' - ' . (string)$command;
+            echo '<br />';
+        }
     }
 
     public function performAction($input)
     {
         echo 'Ваш выбор: ' . $input . '<br />';
-
-        switch ($input) {
-            case '2':
-                $this->turnLightOff();
-                break;
-            case '4':
-                $this->turnMusicOff();
-                break;
-            case '6':
-                $this->turnTvOff();
-                break;
-        }
 
         if (isset($this->commands[$input])) {
             $this->commands[$input]->execute();
@@ -104,27 +107,15 @@ class RemoteControl
     {
         $this->commands[$buttonId] = $cmd;
     }
-
-
-    private function turnLightOff()
-    {
-        echo 'Свет выключен';
-    }
-
-    private function turnMusicOff()
-    {
-        echo 'Музыка выключена';
-    }
-
-    private function turnTvOff()
-    {
-        echo 'Телевизор выключен';
-    }
 }
 
 $control = new RemoteControl();
+$control->setCommandForButton('1', new LightsCommand());
+$control->setCommandForButton('2', new TvCommand());
+$control->setCommandForButton('3', new MusicCommand());
+$control->setCommandForButton('4', new TeapotCommand());
 
 $control->drawMenu();
 echo '<br />';
 echo '<br />';
-$control->performAction('1');
+$control->performAction('4');
