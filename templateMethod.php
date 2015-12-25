56180f6a1663188cf862e51f1a056a97b15afeab
Yurii Pereskokov <skulines@mail.ru>
2015-12-25 17:53:50 +0500

    25.12

--- templateMethod.php
+++ templateMethod.php
@@ -1,7 +1,80 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: user
- * Date: 25.12.2015
- * Time: 9:52
+ *
  */
+/**
+ * templateMethod.php
+ * Реализация паттерна "Шаблонный метод"
+ *
+ * @author      Pereskokov Yurii
+ * @copyright   2015 Pereskokov Yurii
+ * @license     Mediasite LLC
+ * @link        http://www.mediasite.ru/
+ */
+
+class FastFood
+{
+    public function roastBread()
+    {
+        echo 'Bread';
+    }
+
+    public function putVegetables()
+    {
+        echo 'Vegetables';
+    }
+
+    public abstract function prepare();
+}
+
+class Hamburger extends FastFood
+{
+    private function putVegetables()
+    {
+        echo 'Vegetables';
+    }
+
+    private function fryMeat()
+    {
+        echo 'Meat';
+    }
+
+    private function addKetchup()
+    {
+        echo 'Ketchup';
+    }
+
+    private function roastBread()
+    {
+        echo 'Bread';
+    }
+
+    public function prepare()
+    {
+        $this->roastBread();
+        echo '<br />';
+        $this->fryMeat();
+        echo '<br />';
+        $this->putVegetables();
+        echo '<br />';
+        $this->addKetchup();
+        echo '<br />';
+    }
+}
+
+class HotDog extends FastFood
+{
+    public function prepare()
+    {
+        /*
+        $this->roastBread();
+        echo '<br />';
+        $this->fryMeat();
+        echo '<br />';
+        $this->putVegetables();
+        echo '<br />';
+        $this->addKetchup();
+        echo '<br />';
+        */
+    }
+}
